d6b63c7972b7db59a6b07daafd9dda8b3e6ca909
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-28 16:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber (line, names) {
   line.push(names);
   return (`Welcome ${names}, you are ${line.length} in line.`);
 }
+
+function nowServing (katzDeliLine) {
+  if (!katzDeliLine) {
+    return (`)
+  }
+}
